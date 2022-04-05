170c76466f7939ce0741be12cfed79b2711dee8c
patrick-katambi <katambipatrick@gmail.com>
2022-04-05 16:16:08 +0100

    delete user token

--- app/Http/Traits/DbTrait.php
+++ app/Http/Traits/DbTrait.php
@@ -3,6 +3,7 @@
 namespace App\Http\Traits;
 
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 
 trait DbTrait
 {
@@ -14,5 +15,16 @@ trait DbTrait
     {
         $registered_user = User::where('email', $email)->first();
         return $registered_user;
+    }
+
+
+    /**
+     * deleting a token associated with a particular user using passed id
+     */
+
+    protected function deleteUserToken(int $user_id) : int
+    {
+        $recordRemoved = DB::table('personal_access_tokens')->where('tokenable_id', $user_id)->delete();
+        return $recordRemoved;
     }
 }
