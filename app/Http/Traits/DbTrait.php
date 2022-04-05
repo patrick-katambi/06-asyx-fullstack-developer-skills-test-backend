df9dd17678428634082db79d80eb967a026f1766
patrick-katambi <katambipatrick@gmail.com>
2022-04-05 16:10:15 +0100

    database trait helper

---
+++ app/Http/Traits/DbTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Traits;
+
+use App\Models\User;
+
+trait DbTrait
+{
+    /**
+     * identifying a user by email passed, since the email is unique
+     * for all users
+     */
+    protected function queryRegisteredUser(string $email) : User
+    {
+        $registered_user = User::where('email', $email)->first();
+        return $registered_user;
+    }
+}
