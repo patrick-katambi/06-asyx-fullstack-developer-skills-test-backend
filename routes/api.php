5d28d64c8bde09e47d0c7b00325754e8ca46a6a8
patrick-katambi <katambipatrick@gmail.com>
2022-04-06 11:07:46 +0100

    api routes unused imports

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
-use App\Models\Priority;
-use App\Models\UserGroup;
-use Illuminate\Http\Response;
 
 Route::post('/user/register', [UserController::class, 'userRegistration']);
 Route::post('/user/login', [UserController::class, 'userLogin']);
