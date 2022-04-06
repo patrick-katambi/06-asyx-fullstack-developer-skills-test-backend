6a545877788f44ace6738c011136b2f20e749c98
patrick-katambi <katambipatrick@gmail.com>
2022-04-06 11:06:52 +0100

    removing dummy route

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,6 @@ use Illuminate\Http\Response;
 
 Route::post('/user/register', [UserController::class, 'userRegistration']);
 Route::post('/user/login', [UserController::class, 'userLogin']);
-Route::get('/user/stuff', function () {
-    $users = Priority::find(1)->tickets;
-    return response(['user_group-users' => $users]);
-});
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/user/logout/{user_id}', [UserController::class, 'logOut']);
 });
